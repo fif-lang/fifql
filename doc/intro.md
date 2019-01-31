03783065377bdf8b72017526dabeedd8370b61d7
Benjamin Zaporzan <benzaporzan@gmail.com>
2019-01-30 22:12:54 -0500

    First Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to fifql
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/fifql/core_test.clj
@@ -0,0 +1,7 @@
+(ns fifql.core-test
+  (:require [clojure.test :refer :all]
+            [fifql.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
