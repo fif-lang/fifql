d57b04d323ca0a0a255fa83aab60404973e083ed
Benjamin Zaporzan <benzaporzan@gmail.com>
2019-02-09 18:11:26 -0500

    Updated readme

--- dev/fifql/dev/server.clj
+++ dev/fifql/dev/server.clj
@@ -1,49 +1,59 @@
 (ns fifql.dev.server
   (:require
+
+   ;; For managing Clojure App State
    [mount.core :as mount :refer [defstate]]
+
+   ;; High-performance Web Server
    [org.httpkit.server :as httpkit]
+
+   ;; Routing Library
    [compojure.core :refer [GET POST defroutes]]
    [compojure.route :as route]
+   
+   ;; Fifql Library
    [fifql.server :refer [create-ring-request-handler]]
-   [fifql.core :as fifql]
-   [fif.core :as fif]))
+   [fifql.core :as fifql]))
 
 
+(def server-name "A fifql Example Server")
+(def server-port 8080)
+
+
+;; Create our stack machine, and define some word functions
 (def stack-machine
   (-> (fifql/create-stack-machine)
 
       (fifql/set-word 'add2 (fifql/wrap-function 1 (fn [x] (+ 2 x)))
        :doc "(n -- n) Add 2 to the value"
-       :group :test)
+       :group :fifql/example)
 
-      (fifql/set-word 'fail (fifql/wrap-procedure 0 (fn [] (throw (ex-info "Failed" {}))))
-       :doc "Throw an error, stop the stack-machine."
-       :group :test)
-
-      (fifql/set-var 'server-details {:port 8080 :server-name "test server"}
+      (fifql/set-var 'server-details {:server-port server-port :server-name server-name}
        :doc "The server details"
-       :group :test)))
+       :group :fifql/example)))
 
 
+;; Create our ring request handler to use with Httpkit
 (def fifql-handler
   (create-ring-request-handler
-   :prepare-stack-machine
-   (fn [request] stack-machine)
-   :post-response
-   (fn [sm request response] response)))
+   :prepare-stack-machine stack-machine))
 
 
+;; Create our routes. The fifql ring handler supports both GET and POST requests
 (defroutes app
   (GET "/fifql" req fifql-handler)
   (POST "/fifql" req fifql-handler)
   (route/not-found "<h1>Page not Found</h1>"))
 
 
+;; Start the web server. Note that any server that supports ring
+;; request handlers are supported.
 (defn start
   []
-  (httpkit/run-server #'app {:port 8080}))
+  (httpkit/run-server #'app {:port server-port}))
 
 
+;; Manage our http server using Mount (optional)
 (defstate http-server
   :start (start)
   :stop (http-server))
